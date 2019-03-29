b51be0d05f3689505995897e9886fdc845ced3b3
romulo-martins <romulo_martins@id.uff.br>
2019-03-28 21:39:03 -0300

    Terminando a lista 1 de 2019.1

--- 20191/lista1/q1/max_lenght.c
+++ 20191/lista1/q1/max_lenght.c
@@ -1,22 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/*
-	Programa que, infinitamente, le um número inteiro n, que representa o tamanho,
-	seguida de uma sequência de n números inteiros, e determa o comprimento máximo de um
-	segmento crescente destes n números. Exemplos:
- 		Na sequência 5, 10, 3, 2, 4, 7, 9, 8, 5 o comprimento do segmento crescente máximo é 4.
- 		Na sequência 10, 8, 7, 5, 2 o comprimento de um segmento crescente máximo é 1.
-	O programa para quando n for menor ou igual a zero.
-*/
+int max_asc_lenght(int n) {
+	printf("Informe %d números: \n", n);
+	int max = 1, count = 1; 
+	int prev, current;
+
+	scanf("%d", &prev);
+	for (int i = 0; i < n-1; i++) {
+		scanf("%d", &current);
+		if(current > prev) {
+			count++;
+		} else {
+			if (count > max) max = count; 
+			count = 1;
+		}
+		prev = current;
+	}
+	return max;
+}
 
 int main (void) {
 	int n;
 	do {
 		printf("Informe um numero positivo N (negativo para sair): ");
 		scanf("%d",&n);
-		if (n < 0) break;
-		// TODO: implementar função que calcula o comprimento.
+		if (n < 1) break;
+		printf("O comprimento máximo é: %d.\n", max_asc_lenght(n));
 	} while (1);
 
 	printf("### Fim do programa! ###\n");
