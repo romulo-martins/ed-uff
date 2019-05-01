8e34479131d7a13c61a5978a9b1896d011743f61
romulo-martins <romulo_martins@id.uff.br>
2019-05-01 14:56:02 -0300

    Finalizando as questões (b) e (c) da lista 6.

---
+++ lista6/qb.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lib/fila.h"
+#include "lib/pilha.h"
+
+/*
+(b) uma função que, dada uma fila f, retorne uma nova fila, com todos os elementos de f,
+porém invertida em relação a fila f. Não é possível alterar a ordem dos elementos da fila de
+entrada. A função deve obedecer o seguinte protótipo: TFila* inv_fila (TFila *f);
+*/
+
+TFila* inv_fila (TFila *f);
+
+int main(int argc, char const *argv[]) {
+	TFila *f = inicializa_fila();
+	insere(f, 2);
+	insere(f, 3);
+	insere(f, 5);
+	insere(f, 7);
+	insere(f, 11);
+	insere(f, 13);
+
+	TFila *f_inv = inv_fila(f);
+	
+	printf("Fila: \n");
+	imprime_fila(f);
+
+	printf("Fila invertida: \n");
+	imprime_fila(f_inv);
+
+	return 0;
+}
+
+TFila* inv_fila (TFila *f) {
+	TFila *f_temp = inicializa_fila();
+	TPilha *p_temp = inicializa_pilha();
+
+	// Passa os dados para as estruturas temporarias.
+	while(!fila_vazia(f)) {
+		int valor = retira(f);
+		push(p_temp, valor);
+		insere(f_temp, valor);
+	}
+
+	// Passa os dados da pilha para fila invertida.
+	TFila *f_inv = inicializa_fila();
+	while(!pilha_vazia(p_temp)) {
+		int valor = pop(p_temp);
+		insere(f_inv, valor);
+	}
+
+	// Retorna os dados para fila original.
+	while(!fila_vazia(f_temp)) {
+		int valor = retira(f_temp);
+		insere(f, valor);
+	}
+
+	return f_inv; 
+}
---
+++ lista6/qc.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lib/fila.h"
+
+/*
+(c) uma função que, dada uma fila f, retorne uma nova fila contendo os elementos pares de f,
+e modifique a fila original (f) para que ela possua somente os elementos ímpares, respeitando
+a ordem de entrada em f. A função deve obedecer o seguinte protótipo: 
+TFila* sep_fila (TFila *f);
+*/
+
+TFila* sep_fila (TFila *f);
+
+int main(int argc, char const *argv[]) {
+	TFila *f = inicializa_fila();
+	insere(f, 2);
+	insere(f, 3);
+	insere(f, 4);
+	insere(f, 5);
+	insere(f, 6);
+	insere(f, 7);
+	insere(f, 8);
+
+	TFila *f_pares = sep_fila(f);
+	
+	printf("Fila impares: \n");
+	imprime_fila(f);
+
+	printf("Fila pares: \n");
+	imprime_fila(f_pares);
+
+
+	return 0;
+}
+
+TFila* sep_fila (TFila *f) {
+	TFila *f_impares = inicializa_fila();
+	TFila *f_pares = inicializa_fila();
+
+	while(!fila_vazia(f)) {
+		int valor = retira(f);
+		if (valor % 2 == 0) {
+			insere(f_pares, valor);
+		} else {
+			insere(f_impares, valor);
+		}
+	}
+
+	while(!fila_vazia(f_impares)) {
+		int valor = retira(f_impares); 
+		insere(f, valor);
+	}
+
+	return f_pares;
+}
